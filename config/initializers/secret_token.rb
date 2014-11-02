e47802bb579b9a683e222f180ebebee04ac3da40
Nejuf <matthew.b.blankenship@gmail.com>
2014-11-01 21:08:03 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HBServer::Application.config.secret_token = 'd9dd9df2b4fbe928c139f0bd79a7bc7cf39d078ec00dd79ec7079769e8b535685a989c84260aaa942e4b522cb371c8a8d694e15ac20df427c5f98bdd85cbb992'
